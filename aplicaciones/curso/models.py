c8d54ec072d9b92dc80d1222158c2bbbac994e95
Roberto Bravo <robertbass@hotmail.com>
2026-07-14 11:57:04 -0500

    VERSION PREVIEW

--- aplicaciones/curso/models.py
+++ aplicaciones/curso/models.py
@@ -4,6 +4,19 @@ from aplicaciones.docente.models import Docente
 
 # Create your models here.
 class Curso(models.Model):
+    NIVEL_CHOICES = [
+        (1, 'Nivel 1'),
+        (2, 'Nivel 2'),
+        (3, 'Nivel 3'),
+        (4, 'Nivel 4'),
+        (5, 'Nivel 5'),
+        (6, 'Nivel 6'),
+        (7, 'Nivel 7'),
+        (8, 'Nivel 8'),
+        (9, 'Nivel 9'),
+        (10, 'Nivel 10'),
+    ]
+
     JORNADA_CHOICES = [
         ('MATUTINA', 'Matutina'),
         ('VESPERTINA', 'Vespertina'),
@@ -12,7 +25,11 @@ class Curso(models.Model):
 
     nombre = models.CharField(max_length=100, null=False, blank=False)
     creditos = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
-    nivel = models.CharField(max_length=20, null=False, blank=False, validators=[MinValueValidator(1), MaxValueValidator(10)], help_text='Nivel de la malla curricular (1 a 10).')
+    nivel = models.PositiveSmallIntegerField(
+        choices=NIVEL_CHOICES,
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        help_text='Nivel de la malla curricular (1 a 10).',
+    )
     horas_semanales = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(40)]
     )
